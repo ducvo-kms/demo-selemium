976298eef284606fae4de5b24b0c3f741a28ffb0
ducvo <ducvo@kms-technology.com>
2023-02-16 16:37:41 +0700

    test

--- src/test/java/com/ducvo/AppTest.java
+++ src/test/java/com/ducvo/AppTest.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
 /**
  * Unit test for simple App.
@@ -24,7 +26,10 @@ public class AppTest
         System.setProperty(
             "webdriver.chrome.driver",
             "/Users/ducvo/contribute/code/kms-connect/plugins/eclinicalworks/chromedriver");
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox", "--disable-gpu");
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
